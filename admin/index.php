3f7d64191e797dab7431e5637fc5b2c3a4cddf15
syamsifathur <fathur.rachmad45@gmail.com>
2021-03-28 15:42:50 +0700

    coba template sb admin bs

--- admin/index.php
+++ admin/index.php
@@ -15,123 +15,194 @@ if (!isset($_SESSION['admin']))
 <html>
 <head>
       <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Administrator Toko</title>
-    <!-- Bootstrap css core -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-  <link href="assets/css/simple-sidebar.css" rel="stylesheet">
-</head>
-<body>
-
-  <div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Administrator </div>
-      <div class="list-group list-group-flush">
-        <a href="index.php" class="list-group-item list-group-item-action bg-light">Home</a>
-        <a href="index.php?halaman=produk" class="list-group-item list-group-item-action bg-light">Produk</a>
-        <a href="index.php?halaman=pembelian" class="list-group-item list-group-item-action bg-light">Pembelian</a>
-        <a href="index.php?halaman=pelanggan" class="list-group-item list-group-item-action bg-light">Pelanggan</a>
-      </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-          <button class="btn btn-default" id="menu-toggle"><span class="navbar-toggler-icon"></span></button>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="../login.php" target="_blank" rel="nofollow">Login User</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="logout.php">Log Out</a>
-            </li>
-          </ul>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Sidenav Light - SB Admin</title>
+        <link href="assets/css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
+            </form>
+            <!-- Navbar-->
+            <ul class="navbar-nav ml-auto ml-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Activity Log</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="login.html">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <div class="sb-sidenav-menu-heading">Interface</div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                Layouts
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                                    <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                                </nav>
+                            </div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                                Pages
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                        Authentication
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="login.html">Login</a>
+                                            <a class="nav-link" href="register.html">Register</a>
+                                            <a class="nav-link" href="password.html">Forgot Password</a>
+                                        </nav>
+                                    </div>
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                                        Error
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="401.html">401 Page</a>
+                                            <a class="nav-link" href="404.html">404 Page</a>
+                                            <a class="nav-link" href="500.html">500 Page</a>
+                                        </nav>
+                                    </div>
+                                </nav>
+                            </div>
+                            <div class="sb-sidenav-menu-heading">Addons</div>
+                            <a class="nav-link" href="charts.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Charts
+                            </a>
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tables
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Sidenav Light</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Sidenav Light</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-body">
+                                 <?php 
+                                    if (isset($_GET['halaman']))
+                                    {
+                                        if ($_GET['halaman']=="produk")
+                                        {
+                                            include 'produk.php';
+                                        }
+                                        elseif ($_GET['halaman']=="pembelian")
+                                        {
+                                            include 'pembelian.php';
+                                        }
+                                        elseif ($_GET['halaman']=="pelanggan")
+                                        {
+                                            include 'pelanggan.php';
+                                        }
+                                        elseif ($_GET['halaman']=="detail")
+                                        {
+                                            include 'detail.php';
+                                        }
+                                        elseif ($_GET['halaman']=="tambahproduk")
+                                        {
+                                            include 'tambahproduk.php';
+                                        }
+                                        elseif ($_GET['halaman']=="tambahpelanggan") 
+                                        {
+                                            include 'tambahpelanggan.php';
+                                        }
+                                        elseif ($_GET['halaman']=="hapusproduk")
+                                        {
+                                            include 'hapusproduk.php';
+                                        }
+                                        elseif ($_GET['halaman']=="hapuspelanggan")
+                                        {
+                                            include 'hapuspelanggan.php';
+                                        }
+                                        elseif ($_GET['halaman']=="editproduk")
+                                        {
+                                            include 'editproduk.php';    
+                                        }
+                                        elseif ($_GET['halaman']=="editpelanggan")
+                                        {
+                                            include 'editpelanggan.php';
+                                        }
+                                        elseif ($_GET['halaman']=="pembayaran") 
+                                        {
+                                            include 'pembayaran.php';
+                                        }
+                                    }
+                                    else 
+                                    {
+                                        include 'home.php' ;
+                                    }
+                                ?>
+                                                </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
         </div>
-      </nav>
-
-      <div class="container-fluid">
-        
-         <?php 
-                if (isset($_GET['halaman']))
-                {
-                    if ($_GET['halaman']=="produk")
-                    {
-                        include 'produk.php';
-                    }
-                    elseif ($_GET['halaman']=="pembelian")
-                    {
-                        include 'pembelian.php';
-                    }
-                    elseif ($_GET['halaman']=="pelanggan")
-                    {
-                        include 'pelanggan.php';
-                    }
-                    elseif ($_GET['halaman']=="detail")
-                    {
-                        include 'detail.php';
-                    }
-                    elseif ($_GET['halaman']=="tambahproduk")
-                    {
-                        include 'tambahproduk.php';
-                    }
-                    elseif ($_GET['halaman']=="tambahpelanggan") 
-                    {
-                        include 'tambahpelanggan.php';
-                    }
-                    elseif ($_GET['halaman']=="hapusproduk")
-                    {
-                        include 'hapusproduk.php';
-                    }
-                    elseif ($_GET['halaman']=="hapuspelanggan")
-                    {
-                        include 'hapuspelanggan.php';
-                    }
-                    elseif ($_GET['halaman']=="editproduk")
-                    {
-                        include 'editproduk.php';    
-                    }
-                    elseif ($_GET['halaman']=="editpelanggan")
-                    {
-                        include 'editpelanggan.php';
-                    }
-                    elseif ($_GET['halaman']=="pembayaran") 
-                    {
-                        include 'pembayaran.php';
-                    }
-                }
-                else 
-                {
-                    include 'home.php' ;
-                }
-                ?>
-        
-      </div>
-    </div>
-    <!-- /#page-content-wrapper -->
-
-  </div>
-  <!-- /#wrapper -->
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Menu Toggle Script -->
-  <script>
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-    });
-  </script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="assets/js/scripts.js"></script>
    
 </body>
 </html>
