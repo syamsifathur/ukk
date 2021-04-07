cb41d13718fe45aab2d8ba9e77ac6233dfcbed0f
syamsifathur <fathur.rachmad45@gmail.com>
2021-04-07 21:33:36 +0700

    update beli.php jika stock nya 0 maka tidak bisa masuk ke keranjang dan melanjutkan pembayarannya

--- beli.php
+++ beli.php
@@ -1,22 +1,35 @@
 <?php 
 session_start();
+include "admin/koneksi.php";
 // mendapatkan id produk dari url
 $id_produk = $_GET['id'];
 
+// query ambil data dari database
+$ambil = $koneksi->query("SELECT * FROM produk WHERE id_produk='$id_produk'");
+$detail = $ambil->fetch_assoc();
+
+//jika stok produknya kosong maka muncul alert, dan produk tidak bisa masuk ke keranjang
+if ($detail['stock']=="0") {
+	echo "<script>alert('Mohon maaf Stock produk lagi kosong, tenang saja akan segera di restock kok');</script>";
+	echo "<meta http-equiv='refresh' content='1;url=index.php'>";
+}
 //jk sudah ada produk itu dikeranjang, maka produk itu jumlahnya di +1
-if (isset($_SESSION['keranjang'][$id_produk]))
+elseif (isset($_SESSION['keranjang'][$id_produk]))
 {
 	$_SESSION['keranjang'][$id_produk]+=1;
+	echo "<script>alert('Produk berhasil masuk ke Keranjang belanja');</script>";
+	echo "<meta http-equiv='refresh' content='1;url=keranjang.php'>";
 }
 
 //Selainitu (blm ada di keranjang), maka produk itu dianggap dibeli 1
 else 
 {
 	$_SESSION['keranjang'][$id_produk] = 1;
+	//larikan ke halaman keranjang
+	echo "<script>alert('Produk berhasil masuk ke Keranjang belanja');</script>";
+	echo "<meta http-equiv='refresh' content='1;url=keranjang.php'>";
 }
 
 
-//larikan ke halaman keranjang
-echo "<script>alert('Produk berhasil masuk ke Keranjang belanja');</script>";
-echo "<meta http-equiv='refresh' content='1;url=keranjang.php'>";
+
 ?>
